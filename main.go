893a97f46c3750202abd9964ccca67777b9fea7f
The FreeChessClub Author(s) <adk@freechess.club>
2017-02-17 21:09:02 -0800

    Change the app from node to Go.

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+)
+
+var (
+	log         = logrus.WithField("cmd", "go-freechessclub")
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.WithField("PORT", port).Fatal("$PORT must be set")
+	}
+
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.Handle("/chat/", http.FileServer(http.Dir("./chat/public")))
+	http.HandleFunc("/ws", handleWebsocket)
+	log.Println(http.ListenAndServe(":"+port, nil))
+}
