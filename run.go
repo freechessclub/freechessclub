886de63bdabb43bc917ec565fdc5fe70b4b0f1b5
The FreeChessClub Author(s) <adk@freechess.club>
2017-02-17 23:48:33 -0800

    Update vendor dependencies.

--- run.go
+++ run.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"time"
 	"github.com/Sirupsen/logrus"
-	"github.com/heroku/x/redis"
+	"github.com/garyburd/redigo/redis"
 )
 
 var (
@@ -13,12 +13,68 @@ var (
 	rw          redisWriter
 )
 
+// WaitFunc to be executed occasionally by something that is waiting.
+// Should return an error to cancel the waiting
+// Should also sleep some amount of time to throttle connection attempts
+type WaitFunc func(time.Time) error
+
+// WaitForAvailability of the redis server located at the provided url, timeout if the Duration passes before being able to connect
+func WaitForAvailability(url string, d time.Duration, f WaitFunc) (bool, error) {
+	conn := make(chan struct{})
+	errs := make(chan error)
+	go func() {
+		for {
+			c, err := redis.DialURL(url)
+			if err == nil {
+				c.Close()
+				close(conn)
+				return
+			}
+			if f != nil {
+				err := f(time.Now())
+				if err != nil {
+					errs <- err
+					return
+				}
+			}
+		}
+	}()
+	select {
+	case err := <-errs:
+		return false, err
+	case <-conn:
+		return true, nil
+	case <-time.After(d):
+		return false, nil
+	}
+}
+
+// NewRedisPoolFromURL returns a new *redigo/redis.Pool configured for the supplied url
+// The url can include a password in the standard form and if so is used to AUTH against
+// the redis server
+func NewRedisPoolFromURL(url string) (*redis.Pool, error) {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.DialURL(url)
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+	}, nil
+}
+
 func runRedis() {
 	redisURL := os.Getenv("REDIS_URL")
 	if redisURL == "" {
 		log.WithField("REDIS_URL", redisURL).Fatal("$REDIS_URL must be set")
 	}
-	redisPool, err := redis.NewRedisPoolFromURL(redisURL)
+	redisPool, err := NewRedisPoolFromURL(redisURL)
 	if err != nil {
 		log.WithField("url", redisURL).Fatal("Unable to create Redis pool")
 	}
@@ -28,7 +84,7 @@ func runRedis() {
 
 	go func() {
 		for {
-			waited, err := redis.WaitForAvailability(redisURL, waitTimeout, rr.wait)
+			waited, err := WaitForAvailability(redisURL, waitTimeout, rr.wait)
 			if !waited || err != nil {
 				log.WithFields(logrus.Fields{"waitTimeout": waitTimeout, "err": err}).Fatal("Redis not available by timeout!")
 			}
@@ -43,7 +99,7 @@ func runRedis() {
 
 	go func() {
 		for {
-			waited, err := redis.WaitForAvailability(redisURL, waitTimeout, nil)
+			waited, err := WaitForAvailability(redisURL, waitTimeout, nil)
 			if !waited || err != nil {
 				log.WithFields(logrus.Fields{"waitTimeout": waitTimeout, "err": err}).Fatal("Redis not available by timeout!")
 			}
