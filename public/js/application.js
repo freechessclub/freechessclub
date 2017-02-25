3b591903cb98d932b0073e5b30a0e708c49138ee
The FreeChessClub Author(s) <adk@freechess.club>
2017-02-25 00:31:30 -0800

    Implement tabs.

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,8 @@
-var chat;
+var chat = new ReconnectingWebSocket(location.protocol.replace("http","ws") + "//" + location.host + "/ws");
+var board = ChessBoard('board', 'start');
 var connected = false;
 var handle = "";
+var tabs;
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
@@ -13,14 +15,40 @@ $('#collapse-chat').on('show.bs.collapse', function () {
   $("#chat-toggle").text(String.fromCharCode(8211));
 })
 
+jQuery(document.body).on('click', '.closeTab', function(event) {
+  var tabContentId = $(this).parent().attr("href");
+  $(this).parent().remove();
+  delete tabs[tabContentId.substr(1)+"-content"];
+  $('#tabs a:last').tab('show');
+  $(tabContentId).remove();
+});
+
+$(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function (e) {
+  var tab = $(e.target);
+  tab.css('color', 'black');
+});
+
 chat.onmessage = function(message) {
   var data = JSON.parse(message.data);
-  if (connected == false && data.handle != "") {
+  if (connected == false && data.handle != "" && data.type == 0) {
     connected = true;
     handle = data.handle;
     $("#chat-status").text("Connected as " + handle);
-    if (data.text === "") {
+    if (data.text == "") {
       return;
+    }
+  }
+
+  var tab = tabs["ch53"];
+  if (data.type == 2) {
+    if (!tabs.hasOwnProperty(data.handle)) {
+      $('<a class="flex-sm-fill text-sm-center nav-link" data-toggle="tab" href="#'+data.handle+'-content" id="'+data.handle+'" role="tab">'+data.handle+'<span class="btn btn-default btn-sm closeTab">×</span></a>').appendTo('#tabs');
+      $('<div class="tab-pane chat-text" id="'+data.handle+'-content" role="tabpanel"></div>').appendTo('.tab-content');
+      $(".chat-text").height($("#board").height()-115);
+      tab = $("#"+data.handle+"-content");
+      tabs[data.handle] = tab;
+    } else {
+      tab = tabs[data.handle];
     }
   }
 
@@ -29,16 +57,22 @@ chat.onmessage = function(message) {
     textclass = " class=\"mine\"";
   }
 
-  $("#chat-text").append(
-    "<strong"+textclass+">"+$('<span/>').text(data.handle).html()+"</strong>: "+
-    $('<span/>').text(data.text).html()+"</br>");
-  $("#chat-text").scrollTop($("#chat-text")[0].scrollHeight);
+  if (data.type == 1 || data.type == 2 || data.type == 4) {
+    tab.append(
+      "<strong"+textclass+">"+$('<span/>').text(data.handle).html()+"</strong>: "+
+      $('<span/>').text(data.text).html()+"</br>");
+      tabheader = $("#"+data.handle);
+      if (tabheader.hasClass('active')) {
+        tab.scrollTop(tab[0].scrollHeight);
+      } else {
+        tabheader.css('color', 'red');
+      }
+  }
 };
 
 chat.onclose = function(){
   connected = false;
   $("#chat-status").text("Disconnected");
-  this.chat = new WebSocket(chat.url);
 };
 
 $("#input-form").on("submit", function(event) {
@@ -64,12 +98,11 @@ $("#input-form").on("submit", function(event) {
 $(document).ready(function() {
   connected = false;
   $("#chat-status").text("Connecting...");
-  chat = new ReconnectingWebSocket(location.protocol.replace("http","ws") + "//" + location.host + "/ws");
-  var board = ChessBoard('board', 'start');
-  $("#chat-text").height($("#board").height()-55);
+  $(".chat-text").height($("#board").height()-115);
+  tabs = { ch53: $("#ch53-content") };
 });
 
 $(window).resize(function() {
   board.resize();
-  $("#chat-text").height($("#board").height()-55);
+  $(".chat-text").height($("#board").height()-115);
 });
