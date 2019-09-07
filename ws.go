d67eb4655f9368d7ed47c4fcda240b651a36b611
The FreeChessClub Author(s) <hi@freechess.club>
2019-09-07 15:20:02 -0700

    Update copyright year
    Signed-off-by: The FreeChessClub Author(s) <hi@freechess.club>

--- ws.go
+++ ws.go
@@ -1,4 +1,4 @@
-// Copyright © 2017 Free Chess Club <help@freechess.club>
+// Copyright © 2019 Free Chess Club <help@freechess.club>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
